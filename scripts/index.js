6b7d40a33666cc6ffd90cf2ca452e422f837a649
OldManIroh <saetgaraev.airat@gmail.com>
2023-11-06 02:27:09 +0500

    Вроде выполнил верно

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,32 @@
 const cardList = document.querySelector(".places__list");
+
 // @todo: Темплейт карточки
-function addCard(nameCard, srcCard) {
-  const cardTimplate = document.querySelector("#card-template").content;
-  const cardElement = cardTimplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__image").src = srcCard;
-  cardElement.querySelector(".card__title").textContent = nameCard;
-  cardList.append(cardElement);
-  const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
-}
+const cardTimplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(callback, ...cardData) {
+  const cardElement = cardTimplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__title").textContent = cardData[0];
+  cardElement.querySelector(".card__image").src = cardData[1];
+
+  deleteButton.addEventListener("click", callback);
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
 function deleteCard(evt) {
-  evt.target.parentElement.remove();
+  evt.target.closest(".card").remove();
 }
-// @todo: Вывести карточки на страницу
 
-initialCards.forEach(function (item) {
-  addCard(item.name, item.link);
-});
+// @todo: Вывести карточки на страницу
+function renderCard(callback, listWhereFrom, listWhere) {
+  listWhereFrom.forEach(function (item) {
+    listWhere.prepend(createCard(callback, item.name, item.link));
+  });
+}
+
+renderCard(deleteCard, initialCards, cardList);
